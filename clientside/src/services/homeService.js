067ce5082fce3c36ec8fd389a18b98741f948a72
Petersomond1 <petersomond1@gmail.com>
2024-09-02 09:18:47 -0400

    getting features ids done, working on getting the products now

--- clientside/src/services/homeService.js
+++ clientside/src/services/homeService.js
@@ -1,11 +1,15 @@
-// import api from './apiConfig.js';
+import api from './apiConfig.js'
 
-
-
-
+//we create the web page , we gave it a route as a child of the default layout and this is in order
+//to inherit the navbar and sidebar and footer , after this we call the service that will fetch the
+//data or submit it , in the service we handle the logic that will send or get the data
+//now we go to the server side where we search for that endpoint that will trigger a function
+//we go a nd create the function/controller and then after completing the backend setup , we
+//go back o the frontend to handle the data/response coming from the server 
 export async function fetchIdsofProducts() {
     try {
-       const response = await axios.get('http://localhost:3000/api/get-featured-ids');
+       const response = await api.get('/admin/get-featured-ids');
+       return response.data
       
     } catch (error) {
         console.error("Error fetching IDs and products:", error);
