b080b7b52f24ef3cbc046d96d8cbd5ce4e0d25d4
Petersomond1 <petersomond1@gmail.com>
2024-09-04 06:25:35 -0400

    working on the home page : categories and list  card works, home headr needs verification and change in the design

--- clientside/src/main.jsx
+++ clientside/src/main.jsx
@@ -2,10 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+
 import {
-  useQuery,
-  useMutation,
-  useQueryClient,
   QueryClient,
   QueryClientProvider,
 } from 'react-query'
