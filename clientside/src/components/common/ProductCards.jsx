cac06fe3880bf92f584326076fcfb9fe56d0202e
Petersomond1 <petersomond1@gmail.com>
2024-11-11 10:15:14 -0500

    still working on it

---
+++ clientside/src/components/common/ProductCards.jsx
@@ -0,0 +1,23 @@
+// src/components/Card.js
+import React from "react";
+import "../style/homeCategories.css";
+import { Link } from "react-router-dom";
+
+const ProductsCards = ({ secondProductSet }) => {
+  console.log("ham second ", secondProductSet);
+  return secondProductSet?.map((product, index) => (
+    <div className="cardid" key={index}>
+      <h3>{product.name}</h3>
+      <Link to={`/product/${product.name}`}>
+        <img
+          src={product.image_url}
+          alt={product.name}
+          className="card-image"
+        />
+      </Link>
+      {product.description && <p>{product.description}</p>}
+    </div>
+  ));
+};
+
+export default ProductsCards;
