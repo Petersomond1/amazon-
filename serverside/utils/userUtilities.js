ca7589ef3057b5bc4505badaa682c93296b62f87
Petersomond1 <petersomond@gmail.com>
2024-12-24 09:04:10 -0500

    change folder order

--- serverside/utils/userUtilities.js
+++
@@ -1,8 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-// Utility function to create a token
-export const createToken = (user) => {
-    return jwt.sign({ id: user.id, name:user.name, email: user.email, role: user.role }, process.env.JWT_SECRET, {
-        expiresIn: '1h',
-    });
-};
